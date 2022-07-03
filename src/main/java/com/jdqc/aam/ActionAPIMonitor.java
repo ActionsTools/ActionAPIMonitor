bf5555002cfd02b439c410fb9ce602d053bcc8e9
Yujie Liu <yujie.liu.home@gmail.com>
2022-07-03 10:36:07 +0200

    [AAM-2] Init Springboot

---
+++ src/main/java/com/jdqc/aam/ActionAPIMonitor.java
@@ -0,0 +1,12 @@
+package com.jdqc.aam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionAPIMonitor {
+
+    public static void main(String[] args) {
+       SpringApplication.run(ActionAPIMonitor.class, args);
+    }
+}
